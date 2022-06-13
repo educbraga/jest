fee8956724fc45c90d40134c2b1186303d2b5287
educbraga <eduardocbraga@hotmail.com>
2022-06-13 16:37:07 -0300

    Implementa testes para carrinho de compras

---
+++ modulo1/src/lib/Cart.js
@@ -0,0 +1,24 @@
+import find from 'lodash/find';
+import remove from 'lodash/remove';
+
+export default class Cart {
+  items = [];
+
+  add(item) {
+    const itemToFind = { product: item.product };
+    if (find(this.items, itemToFind)) {
+      remove(this.items, itemToFind);
+    }
+    this.items.push(item);
+  }
+
+  remove(product) {
+    remove(this.items, { product });
+  }
+
+  getTotal() {
+    return this.items.reduce((total, item) => {
+      return total + item.product.price * item.quantity;
+    }, 0);
+  }
+}
---
+++ modulo1/src/lib/Cart.spec.js
@@ -0,0 +1,63 @@
+import Cart from './Cart.js';
+
+describe('Cart', () => {
+  let cart;
+  let product = {
+    title: 'Adidas running shoes - men',
+    price: 35388, // 353.88 // R$ 383,88
+  };
+  let product2 = {
+    title: 'Adidas running shoes - women',
+    price: 45388, // 453.88 // R$ 483,88
+  };
+
+  beforeEach(() => {
+    cart = new Cart();
+  });
+
+  describe('getTotal()', () => {
+    it('should return 0 when getTotal() is executed in a newly created instance', () => {
+      expect(cart.getTotal()).toEqual(0);
+    });
+
+    it('should multiply quantity and price and receive the total amount', () => {
+      const item = {
+        product,
+        quantity: 2,
+      };
+
+      cart.add(item);
+
+      expect(cart.getTotal()).toEqual(70776);
+    });
+
+    it('should ensure no more than one product exists at a time', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product,
+        quantity: 1,
+      });
+
+      expect(cart.getTotal()).toEqual(35388);
+    });
+
+    it('should update total when a product gets included and then removed', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+      cart.add({
+        product: product2,
+        quantity: 1,
+      });
+
+      cart.remove(product);
+
+      expect(cart.getTotal()).toEqual(45388);
+    });
+  });
+});
